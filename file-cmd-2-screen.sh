c415a7f87b340e2ef4c181d49687bd777cebcd22
Karl Chen <kchen@sonicwall.com>
2019-08-20 19:44:20 -0700

    updates with new parameter for file-cmd-2-screen.sh

--- file-cmd-2-screen.sh
+++ file-cmd-2-screen.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 #
 
-DEST_SCR_WINDOW=0 
+DEST_SCR_WINDOW="" 
 CMD_DELAY_SEC=2
 LINE_NUM=0
 
-if [ -z "$1" ] ; then 
+if [ -z "${1}" ] ; then 
     echo -e "\nERROR: please specify input filename\n" 
     exit 1
+fi 
+
+if [ ! -z "${2}" ] ; then 
+    DEST_SCR_WINDOW="${2}"         
 fi 
 
 INPUT_CMD_FILE="${1}"
@@ -21,34 +25,16 @@ LINE_NUM=0
 while IFS= read -r line_read
 do
     LINE_NUM=$((LINE_NUM+1))
+#
+# without xargs, cmd file need "\\\$" instead of "\\$", not sure why?  
+#
     line=`printf "%s\n" ${line_read} | xargs`
-    ./send-cmd-2-screen.sh "${line}"
+    ./send-cmd-2-screen.sh "${line}" "${DEST_SCR_WINDOW}"
     if [ $? -ne 0 ] ; then 
        echo -e "\nERROR: LINE #${LINE_NUM}, send '${line_read}' failed!\n"
        exit 3
     fi 
     sleep ${CMD_DELAY_SEC}
 done < "${INPUT_CMD_FILE}"
-exit 0 
-
-LINE_NUM=0
-while IFS= read -r line_read
-do
-    LINE_NUM=$((LINE_NUM+1))
-    line=`printf "%s\n" ${line_read} | xargs`
-    if [ "${line}" != "" ] ; then 
-	if [[ ${line} =~ ^#.* ]] ; then 
-	    continue 
-	fi 
-        printf "\'@ %d, %s\'\n" ${LINE_NUM} "${line}" 
-	./out-usb-console.sh "${line}"
-	if [ $? -ne 0 ] ; then 
-	    echo -e "\nERROR: LINE #${LINE_NUM}, send '${line}' failed!\n"
-	    exit 3
-	fi 
-	sleep 3
-    fi 
-done < "${INPUT_CMD_FILE}"
-exit 0 
 
 
--- send-cmd-2-screen.sh
+++ send-cmd-2-screen.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 #
-echo -e "$1"
-sudo screen -S usb-console -X stuff "${1}\n"
-if [ $? -ne 0 ] ; then 
-    echo -e "ERROR: send to screen failed!"
-    exit 2
+
+if [ -z "${2}" ] ; then 
+    echo -e "${1}"
+    sudo screen -S usb-console -X stuff "${1}\n"
+    if [ $? -ne 0 ] ; then 
+        echo -e "ERROR: send to screen failed!"
+        exit 2
+    fi
+else 
+    echo -e "${1}"
+    sudo screen -S usb-console -p "${2}" -X stuff "${1}\n"
+    if [ $? -ne 0 ] ; then 
+        echo -e "ERROR: send to screen failed!"
+        exit 2
+    fi
 fi

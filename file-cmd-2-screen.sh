048e0ff265c6cc24b2872d7398c831643ea732b4
Karl Chen <kchen@sonicwall.com>
2019-08-22 21:23:36 -0700

    new update for chmage/swap of parameters

--- file-cmd-2-screen.sh
+++ file-cmd-2-screen.sh
@@ -57,42 +57,42 @@ do
 #
     line=`printf "%s\n" "${line_read}" | xargs`
 #    line=`printf "%s\n" "${line_read}"`
-    if [[ ${line} =~ ^#.* ]] ; then 
-        ARG1="`echo "${line}" | awk '{print $1}'`"
-	ARG2="`echo "${line}" | awk '{print $2}'`"
-	ARG3="`echo "${line}" | awk '{print $3}'`"
-	ARG4="`echo "${line}" | awk '{print $4}'`"
-	ARG_REMAIN="`echo "${line}" | awk '{print $5 " " $6 " " $7 " " $8 " " $9}'`"
-	b_do_line=0  
-        if [ "${ARG1}" = "#@" ] ; then 
-	    if [ "${ARG2}" = "IF_board_name" ] ; then 
-		if [[ ( "${ARG3}" = "not" || "${ARG3}" = "!" ) ]] ; then 
-		    if [ ! "${ARG4}" = "${BOARD_NAME}" ] ; then 
-			# printf "%s\n"  "${ARG_REMAIN}"
-			line="`printf "%s\n" "${ARG_REMAIN}"`"
+    if [ ! -z "${line}" ] ; then 
+        if [[ ${line} =~ ^#.* ]] ; then 
+            ARG1="`echo "${line}" | awk '{print $1}'`"
+	    ARG2="`echo "${line}" | awk '{print $2}'`"
+	    ARG3="`echo "${line}" | awk '{print $3}'`"
+	    ARG4="`echo "${line}" | awk '{print $4}'`"
+	    ARG_REMAIN="`echo "${line}" | awk '{print $5 " " $6 " " $7 " " $8 " " $9}'`"
+	    b_do_line=0  
+            if [ "${ARG1}" = "#@" ] ; then 
+		if [ "${ARG2}" = "IF_board_name" ] ; then 
+		    if [[ ( "${ARG3}" = "not" || "${ARG3}" = "!" ) ]] ; then 
+			if [ ! "${ARG4}" = "${BOARD_NAME}" ] ; then 
+			    # printf "%s\n"  "${ARG_REMAIN}"
+			    line="`printf "%s\n" "${ARG_REMAIN}"`"
+			    b_do_line=1
+			fi
+		    elif [ "${ARG3}" = "${BOARD_NAME}" ] ; then 
+			# printf "%s %s\n" "${ARG3}" "${ARG_REMAIN}"
+			line="`printf "%s %s\n" "${ARG4}" "${ARG_REMAIN}"`"
 			b_do_line=1
-		    fi
-		elif [ "${ARG3}" = "${BOARD_NAME}" ] ; then 
-		    # printf "%s %s\n" "${ARG3}" "${ARG_REMAIN}"
-		    line="`printf "%s %s\n" "${ARG4}" "${ARG_REMAIN}"`"
-		    b_do_line=1
-		fi      
-	    elif [ "${ARG2}" = "SHELL_host_cmd" ] ; then 
-		SHELL_CMD="${ARG3} ${ARG4} ${ARG_REMAIN}"
-		printf "\nINFO: #@ SHELL_host_cmd: %s\n" "${SHELL_CMD}"
-		${SHELL_CMD}
+		    fi      
+		elif [ "${ARG2}" = "SHELL_host_cmd" ] ; then 
+		    SHELL_CMD="${ARG3} ${ARG4} ${ARG_REMAIN}"
+		    printf "\nINFO: #@ SHELL_host_cmd: %s\n" "${SHELL_CMD}"
+		    ${SHELL_CMD}
+		fi 
+	    fi 
+	    if [ ${b_do_line} -eq 0 ] ; then 
+		continue
 	    fi 
 	fi 
-	if [ ${b_do_line} -eq 0 ] ; then 
-	    continue
-	fi 
+        ./send-cmd-2-screen.sh "${line}" "${DEST_SCR_WINDOW}"
+        if [ $? -ne 0 ] ; then 
+           echo -e "\nERROR: LINE #${LINE_NUM}, send '${line_read}' failed!\n"
+           exit 3
+        fi 
+        sleep ${CMD_DELAY_SEC}
     fi 
-    ./send-cmd-2-screen.sh "${line}" "${DEST_SCR_WINDOW}"
-    if [ $? -ne 0 ] ; then 
-       echo -e "\nERROR: LINE #${LINE_NUM}, send '${line_read}' failed!\n"
-       exit 3
-    fi 
-    sleep ${CMD_DELAY_SEC}
 done < "${INPUT_CMD_FILE}"
-
-

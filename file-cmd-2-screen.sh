99aabd3aab4370404a176682266336de1693dc70
Karl Chen <kchen@sonicwall.com>
2020-04-04 23:27:14 -0700

    GEN7-5399 update argument list passing

--- file-cmd-2-screen.sh
+++ file-cmd-2-screen.sh
@@ -8,21 +8,20 @@ LINE_NUM=0
 # echo -e "\n DEBUG: ARG_LIST=${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9}\n" 
 
 if [ -z "${1}" ] ; then 
-    echo -e "\nERROR: please specify input filename\n" 
+    echo -e "\nERROR1: please specify input filename\n" 
     exit 1
 fi 
-
-if [ -z "${2}" ] ; then 
-    printf "\nERROR: need screen's destination windows input as 2nd argument!\n"
-    exit 2    
-fi 
-
 INPUT_CMD_FILE="${1}"
- 
 if [ ! -f  ${INPUT_CMD_FILE} ] ; then 
-    echo -e "\nERROR: cannot find input file: ${INPUT_CMD_FILE}\n" 
-    exit 3
+    echo -e "\nERROR2: cannot find input file: ${INPUT_CMD_FILE}\n" 
+    exit 2
 fi  
+
+shift 1
+if [ -z "${1}" ] ; then 
+    printf "\nERROR3: need screen's destination windows input as 2nd argument!\n"
+    exit 3    
+fi 
 
 BOARD_NAME_ARRAY[0]=""
 BOARD_NAME_ARRAY[1]=""
@@ -34,6 +33,13 @@ BOARD_NAME_ARRAY[6]=""
 BOARD_NAME_ARRAY[7]=""
 BOARD_NAME_ARRAY[8]=""
 BOARD_NAME_ARRAY[9]=""
+BOARD_NAME_ARRAY[9]=""
+BOARD_NAME_ARRAY[10]=""
+BOARD_NAME_ARRAY[11]=""
+BOARD_NAME_ARRAY[12]=""
+BOARD_NAME_ARRAY[13]=""
+BOARD_NAME_ARRAY[14]=""
+BOARD_NAME_ARRAY[15]=""
 
 SRC_LOG_FILE_ARRAY[0]=""
 SRC_LOG_FILE_ARRAY[1]=""
@@ -45,9 +51,18 @@ SRC_LOG_FILE_ARRAY[6]=""
 SRC_LOG_FILE_ARRAY[7]=""
 SRC_LOG_FILE_ARRAY[8]=""
 SRC_LOG_FILE_ARRAY[9]=""
+SRC_LOG_FILE_ARRAY[9]=""
+SRC_LOG_FILE_ARRAY[10]=""
+SRC_LOG_FILE_ARRAY[11]=""
+SRC_LOG_FILE_ARRAY[12]=""
+SRC_LOG_FILE_ARRAY[13]=""
+SRC_LOG_FILE_ARRAY[14]=""
+SRC_LOG_FILE_ARRAY[15]=""
+
+SCR_WIN_LIST="$@"
 
 WIN_INDEX=0
-for CUR_WIN in ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} 
+for CUR_WIN in ${SCR_WIN_LIST}
 do
     SRC_LOG_FILE=~/uc-log/uc${CUR_WIN}/uc${CUR_WIN}.log
     if [ ! -f ${SRC_LOG_FILE} ] ; then 
@@ -65,7 +80,7 @@ do
 done
 
 sleep 1
-./send-cmd-2-screen.sh "\n" ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} 
+./send-cmd-2-screen.sh "\n" ${SCR_WIN_LIST}
 if [ $? -ne 0 ] ; then 
    echo -e "\nERROR: send check to all screen windows failed!\n"
    exit 6
@@ -73,7 +88,7 @@ fi
 sleep 2
 
 WIN_INDEX=0
-for CUR_WIN in ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} 
+for CUR_WIN in ${SCR_WIN_LIST}
 do
 #
     BOARD_NAME="`cat ${SRC_LOG_FILE_ARRAY[ ${WIN_INDEX} ]} | tail -n1`"
@@ -86,7 +101,7 @@ done
 
 WIN_INDEX=0
 printf "\n=== INFO of screen Board Name and Log Filename: ===\n"
-for CUR_WIN in ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} 
+for CUR_WIN in ${SCR_WIN_LIST}
 do
     printf "Screen Window Number:%d, BOARD_NAME:\"%s\", LOG_FILE:\"%s\"\n" ${WIN_INDEX} "${BOARD_NAME_ARRAY[ ${WIN_INDEX} ]}" "${SRC_LOG_FILE_ARRAY[ ${WIN_INDEX} ]}"
     WIN_INDEX=$((WIN_INDEX+1))
@@ -108,10 +123,10 @@ do
 	    ARG2="`echo "${line}" | awk '{print $2}'`"
 	    ARG3="`echo "${line}" | awk '{print $3}'`"
 	    ARG4="`echo "${line}" | awk '{print $4}'`"
-	    ARG_REMAIN="`echo "${line}" | awk '{print $5 " " $6 " " $7 " " $8 " " $9 " " $10 " " $11 " " $12 " " $13}'`"
+	    ARG_REMAIN="`echo "${line}" | awk '{print $5 " " $6 " " $7 " " $8 " " $9 " " $10 " " $11 " " $12 " " $13 " " $14 " " $15 " " $16}'`"
             if [ "${ARG1}" = "#@" ] ; then 
                 WIN_INDEX=0
-                for CUR_WIN in ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9}
+                for CUR_WIN in ${SCR_WIN_LIST}
                 do
 		    b_do_line=0  
 		    if [ "${ARG2}" = "IF_board_name" ] ; then 
@@ -145,10 +160,12 @@ do
 	    fi 
         else 
             if [[ ${line} =~ ^@.* ]] ; then 
+# if first character is @, it means silent, no echo print, but this is unesless currently as command pass to screen cannot be silent 
+# remove the @ before pass it to calling script  
                 new_line="`echo ${line} | cut -c2-`"
-                ./send-cmd-2-screen.sh --silent "${new_line}" ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11} ${12} ${13}
+                ./send-cmd-2-screen.sh --silent "${new_line}" ${SCR_WIN_LIST}
             else 
-                ./send-cmd-2-screen.sh "${line}" ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11} ${12} ${13}
+                ./send-cmd-2-screen.sh "${line}" ${SCR_WIN_LIST}
             fi
             if [ $? -ne 0 ] ; then 
                 echo -e "\nERROR: LINE #${LINE_NUM}, send '${line_read}' failed!\n"
